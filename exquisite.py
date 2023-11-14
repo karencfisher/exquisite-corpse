1bea30a756c25a5992ccc86988e7f05d2a7b7909
Dan Wilcox <dan.wilcox@zkm.de>
2023-11-14 20:20:23 +0100

    added option to blank text between folds via 'unfolding', added option to add page break between entry sections and tag writer as <human> or <ai>, added options to control randomness and using dummy ai text, added docstrings for most functions and classes, various improvements on UI configuration including binding seapration based on mode, set default file name when saving, refactored usable bits into util functions

--- exquisite.py
+++ exquisite.py
@@ -1,7 +1,9 @@
-#! /usr//bin/env python3
+#! /usr/bin/env python3
+# Copyright (c) 2023 karencfisher, et al.
 
 import json, random
-import os, sys
+import os
+import sys
 
 import tkinter as tk
 from tkinter import filedialog
@@ -13,10 +15,25 @@ from dotenv import load_dotenv
 
 from poem import Poem
 
-
 class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
+
+        # state
+        self.poem = Poem()
+        self.ignore_first = False # ignore the first line when folding?
+        self.isfolded = False # is the poem folded? then text hidden
+
+        # options
+        self.opt_textsize = 24 # textbox font size
+        self.opt_unfold = True # add interim "unfolded" state to hide previous input, ie. when used within a group
+        self.opt_random = True # randomly choose between ai and human when folding?
+        self.opt_random_ai = 50 # percent chance to get an ai response
+        self.opt_dummy_ai = True # use dummy ai text instead of the OpenAI API?
+        self.opt_breaks = True # force line break between folds
+        self.opt_tags = True # prepend writer tags per fold: <ai> or <human>
+
+        # configure window
         self.master = master
         self.master.title("Exquisite Corpse!")
         self.master.geometry("600x600")
@@ -27,120 +44,127 @@ class Application(tk.Frame):
         load_dotenv()
         openai.api_key = os.getenv('OPENAI_API_KEY')
 
-        # configuration
+        # cloaf onfiguration
         with open('gpt_config.json', 'r') as CONFIG:
             self.config = json.load(CONFIG)
 
-        # system prompt
+        # load system prompt
         with open('instructions.txt', 'r') as INSTRUCT:
             self.instructs = INSTRUCT.read()
-        
-        self.poem = Poem()
-        self.first = True
 
     def create_widgets(self):
-        # Text box
-        self.textbox = tk.Text(self.master, font=Font(family="TkDefaultFont", size=24))
+        """ Create window widgets: textbox & buttons. """
+
+        # text box
+        self.textbox = tk.Text(self.master, font=Font(family="TkDefaultFont", size=self.opt_textsize),
+                               bg="white", highlightthickness=0)
         self.textbox.grid(row=0, column=0, columnspan=5, sticky="nsew", padx=10, pady=10)
         self.master.grid_columnconfigure(0, weight=1)
         self.master.grid_rowconfigure(0, weight=1)
         
-        # AI button
-        self.ai_button = tk.Button(self.master, text="Fold AI", command=self.add_ai_line)
-        self.ai_button.grid(row=1, column=1, sticky="se", padx=(10, 4), pady=10)
+        # fold button
+        self.fold_button = tk.Button(self.master, text="Fold",
+                                     command=(self.toggle_fold if self.opt_unfold else self.fold_poem))
+        self.fold_button.grid(row=1, column=1, sticky="se", padx=(4, 10), pady=10)
 
-        # Human button
-        self.human_button = tk.Button(self.master, text="Fold Human", command=self.add_human_line)
-        self.human_button.grid(row=1, column=2, sticky="se", padx=(0, 4), pady=10)
-
-        # Random Button
-        self.random_button = tk.Button(self.master, text="Fold Random", command=self.add_random_line)
-        self.random_button.grid(row=1, column=3, sticky="se", padx=(0, 10), pady=10)
-
-        # Reveal poem button
+        # reveal poem button
         self.reveal_button = tk.Button(self.master, text="Reveal Poem", command=self.reveal_poem)
         self.reveal_button.grid(row=1, column=0, sticky="sw", padx=(10, 4), pady=10)
-        #self.master.grid_rowconfigure(1, weight=1)
 
-        # Menu
+        # menu
         modkey = "Command" if sys.platform == "darwin" else "Control"
         menubar = tk.Menu(self.master)
         filemenu = tk.Menu(menubar, tearoff=0)
-        filemenu.add_command(label="Fold AI", command=self.add_ai_line, accelerator=f"{modkey}-f")
-        filemenu.add_command(label="Fold Human", command=self.add_human_line, accelerator=f"Shift-{modkey}-F")
-        filemenu.add_command(label="Fold Random", command=self.add_random_line, accelerator=f"{modkey}-R")
+        if self.opt_unfold:
+            filemenu.add_command(label=("Un/Fold"), command=self.toggle_fold, accelerator=f"{modkey}-f")
+        else:
+            filemenu.add_command(label=("Fold"), command=self.fold_poem, accelerator=f"{modkey}-f")
+        filemenu.add_command(label="Reveal Poem", command=self.reveal_poem, accelerator=f"{modkey}-r")
         filemenu.add_separator()
-        filemenu.add_command(label="Save...", command=self.save_file, accelerator=f"{modkey}-S")
+        filemenu.add_command(label="Save...", command=self.save_poem, accelerator=f"{modkey}-S")
         filemenu.add_command(label="Clear...", command=self.clear_poem, accelerator=f"Shift-{modkey}-C")
-        filemenu.add_separator()
-        filemenu.add_command(label="Exit", command=self.exit_app)
+        if sys.platform == "darwin": # override default quit on macOS
+            self.master.createcommand("::tk::mac::Quit", self.exit_app)
+        else: # add menu item
+            filemenu.add_separator()
+            filemenu.add_command(label="Exit", command=self.exit_app)
         menubar.add_cascade(label="File", menu=filemenu)
         viewmenu = tk.Menu(menubar, tearoff=0) # for fullscreen menu option
         menubar.add_cascade(label="View", menu=viewmenu)
         self.master.config(menu=menubar)
 
-        # Bindings
-        self.master.bind_all(f"<{modkey}-f>", self.add_ai_line)
-        self.master.bind_all(f"<{modkey}-F>", self.add_human_line)
-        self.master.bind_all(f"<{modkey}-r>", self.add_random_line)
-        self.master.bind_all(f"<{modkey}-s>", self.save_file)
+        # key bindings
+        self.master.bind_all(f"<{modkey}-s>", self.save_poem)
         self.master.bind_all(f"<{modkey}-C>", self.clear_poem)
+        self.bind_for_folding()
 
-    def save_file(self, event=None):
+        self.textbox.focus_set()
+
+    def bind_for_folding(self):
+        """ Update key bindings for folding, ie. input. """
+        modkey = "Command" if sys.platform == "darwin" else "Control"
+        self.master.bind_all(f"<{modkey}-f>", self.toggle_fold if self.opt_unfold else self.fold_poem)
+        self.master.bind_all(f"<{modkey}-r>", self.reveal_poem)
+
+    def bind_for_reveal(self):
+        """ Update key bindings for the poem reveal, ie. display. """
+        modkey = "Command" if sys.platform == "darwin" else "Control"
+        self.master.unbind(f"<{modkey}-f>")
+        self.master.unbind(f"<{modkey}-r>")
+
+    def save_poem(self, event=None):
+        """ Open save dialog and display selected file in textbox. """
         poem_text = self.poem.get_poem()
-        # Open file dialog and display selected file in textbox
         files = [('Text Document', '*.txt')]
-        file_path = filedialog.asksaveasfilename(filetypes=files, defaultextension=files)
-        with open(file_path, 'w') as FILE:
-            FILE.write(poem_text)
-        self.poem.dirty = False
+        file_path = filedialog.asksaveasfilename(filetypes=files, defaultextension=files,
+                                                 title="Save Poem", initialfile="poem.txt")
+        self.poem.save_to(file_path)
 
     def clear_poem(self, event=None):
+        """ Clear poem and reset for next input. """
         if self.poem.dirty:
-            answer = messagebox.askyesno("Exquisite-corpse", "Would you like to save your poem?")
-            if answer == True:
-                self.save_file()
+            self.ask_to_save_poem()
         self.poem.clear_poem()
-        self.textbox.delete("1.0", tk.END)
-        self.ai_button['state'] = tk.NORMAL
-        self.human_button['state'] = tk.NORMAL
-        self.random_button['state'] = tk.NORMAL
+        self.clear_text()
+        self.fold_button['state'] = tk.NORMAL
         self.reveal_button.configure(text="Reveal Poem", command=self.reveal_poem)
-        self.first = True
+        self.ignore_first = False
+        self.bind_for_folding()
+        self.textbox.focus_set()
 
     def exit_app(self, event=None):
+        """ Ask to save before quitting. """
         if self.poem.dirty:
-            answer = messagebox.askyesno("Exquisite-corpse", "Would you like to save your poem?")
-            if answer == True:
-                self.save_file()
+            self.ask_to_save_poem()
         self.master.quit()
 
-    def add_human_line(self, event=None):
-        # Add next line of poetry from text box
-        user_text = self.textbox.get("1.0", "end-1c")
+    def add_human_lines(self):
+        """ Add current text lines into the poem. """
+        user_text = self.get_text()
+        if user_text == "":
+            return False
+        if self.opt_breaks: self.poem.add_break()
+        if self.opt_tags: self.poem.add_lines("<human>")
+        self.poem.add_lines(user_text, ignore_first=self.ignore_first)
+        return True
 
-        self.poem.add_lines(user_text, first=self.first)
-        self.first = False
-        self.textbox.delete("1.0", tk.END)
-        self.textbox.insert("1.0", self.poem.get_last_line())
+    def add_ai_lines(self):
+        """ Generate the next lines of poetry using ChatGPT. """
 
-    def add_ai_line(self, event=None):
-        # Generate the next line of poetry using GPT-4
-        user_text = self.textbox.get("1.0", "end-1c")
+        # add any text entered before revealing
+        if not self.add_human_lines(): return
 
-        self.poem.add_lines(user_text, first=self.first)
-        self.first = False
-        text = "roses are red\nviolets blue\nbluebirds are great\nand so are you"
-        self.poem.add_lines(text, first=True)
-        self.textbox.delete("1.0", tk.END)
-        self.textbox.insert("1.0", self.poem.get_last_line())
-        return
-
-        self.poem.add_lines(user_text, first=self.first)
+        # add dummy ai poem text
+        if self.opt_dummy_ai:
+            if self.opt_breaks: self.poem.add_break()
+            if self.opt_tags: self.poem.add_lines("<ai>")
+            dummy = "Roses are red\n Violets blue,\nSugar is sweet\n And so are you."
+            self.poem.add_lines(dummy)
+            return True
+        
+        # send prompt for ChatGPT repsonse
         prompt = [{'role': 'system', 'content': self.instructs}]
-        self.first = False
         prompt.append(self.poem.get_prompt())
-
         try:
             response = openai.ChatCompletion.create(
                 model=self.config['model'],
@@ -154,39 +178,94 @@ class Application(tk.Frame):
             )
         except openai.error.AuthenticationError:
             messagebox.showerror('Exquisite-corpse',
-                                 'You have not setup your secret key!')
+                                 'You have not set up your OpenAI secret key!')
             self.master.quit()
-        except:
+        except Exception as exc:
             messagebox.showerror('Exquisite-corpse',
-                                 'An error has occured!')
-            return
-
+                                 f"An error has occured!\n{exc}")
+            return False
         text = response.choices[0].message.content.strip()
         self.poem.add_lines(text)
-        self.textbox.delete("1.0",tk.END)
-        self.textbox.insert("1.0", self.poem.get_last_line())
+        return True
 
-    def add_random_line(self, event=None):
-        # randomly choose between human or ChatGPT 80/20 for next line
-        if(random.randint(0, 100) < 20):
-            self.add_ai_line()
+    def toggle_fold(self, event=None):
+        """
+        Toggle between folded and unfolded states:
+        * folded: fold current text into the poem, hide last line, disable input
+        * unfolded: show last line, ready for next input
+        """
+        self.isfolded = not self.isfolded
+        if self.isfolded:
+            self.fold_poem()
+            self.clear_text() # hide last line
+            self.ignore_first = False
+            self.fold_button.configure(text="Unfold")
+            self.textbox['state'] = tk.DISABLED
+            self.textbox.config(bg=self.master.cget("bg"))#"#eee")
         else:
-            self.add_human_line()
+            self.unfold_poem()
+            self.fold_button.configure(text="Fold")
+            self.textbox.config(bg="white")
+
+    def fold_poem(self, event=None):
+        """
+        Fold current text into the poem. Leave last line visible.
+
+        If opt_random = True, randomly chooses between human or ChatGPT for the
+        next lines, otherwise always generate a ChatGPT response.
+        """
+        if self.opt_random:
+            if(random.randint(0, 100) < self.opt_random_ai):
+                if not self.add_ai_lines(): return
+            else:
+                if not self.add_human_lines(): return
+        else:
+            # ChatGPT
+            if not self.add_ai_line(): return
+        self.set_text(self.poem.get_last_line())
+        self.ignore_first = True
+
+    def unfold_poem(self, event=None):
+        """ Unfold text to show last poem line. """
+        self.textbox['state'] = tk.NORMAL
+        self.set_text(self.poem.get_last_line())
+        self.ignore_first = True
 
     def reveal_poem(self, event=None):
+        """ Show the full poem. """
 
-        # Add any prev text
-        user_text = self.textbox.get("1.0", "end-1c")
-        self.poem.add_lines(user_text, first=self.first)
+        # add any prev text to the poem
+        self.textbox['state'] = tk.NORMAL
+        self.textbox.config(bg="#fff")
+        linecount = len(self.get_text().strip().split('\n'))
+        if (self.ignore_first and linecount > 1) or \
+           (not self.ignore_first and linecount > 0):
+           self.add_human_lines()
 
-        # Show the full current poem
-        self.textbox.delete("1.0",tk.END)
-        self.textbox.insert("1.0", self.poem.get_poem())
-        self.ai_button['state'] = tk.DISABLED
-        self.human_button['state'] = tk.DISABLED
-        self.random_button['state'] = tk.DISABLED
+        # show the poem
+        self.set_text(self.poem.get_poem())
+        self.fold_button['state'] = tk.DISABLED
         self.reveal_button.configure(text="Clear Poem", command=self.clear_poem)
-        
+        self.bind_for_reveal()
+
+    def get_text(self):
+        """ Get current text in the text box. """
+        return self.textbox.get("1.0", "end-1c")
+
+    def clear_text(self):
+        """ Clear text in the text box. """
+        self.textbox.delete("1.0", tk.END)
+
+    def set_text(self, text):
+        """ Set text in the text box. """
+        self.textbox.delete("1.0", tk.END)
+        self.textbox.insert("1.0", text)
+
+    def ask_to_save_poem(self):
+        """ Open a dialog asking to save poem. If yes, then open save dialog. """
+        if messagebox.askyesno("Exquisite-corpse",
+                               "Would you like to save your poem?"):
+            self.save_poem()
 
 def main():
     root = tk.Tk()
--- poem.py
+++ poem.py
@@ -1,12 +1,20 @@
+# Copyright (c) 2023 karencfisher, et al.
+
+import sys
+
 class Poem:
+    """ A sequence of string text lines. """
+
     def __init__(self):
-        self.dirty=False
-        self.complete_text = []
+        self.dirty = False # has the poem changed since the last save?
+        self.complete_text = [] # complete poem as an array of separate lines
 
     def get_last_line(self):
-        return self.complete_text[-1]
+        """ Returns the last line of the poem. """
+        return self.complete_text[-1] if len(self.complete_text) > 0 else "" 
     
     def get_prompt(self):
+        """ Returns a prompt wit hthe last line of the poem for ChatGPT. """
         if len(self.complete_text) == 0:
             self.complete_text.append('\n')
         last_line = self.complete_text[-1]
@@ -14,15 +22,40 @@ class Poem:
         return prompt
     
     def get_poem(self):
+        """ Returns the complete poem as a string. """
         return ''.join(self.complete_text).lstrip()
     
     def clear_poem(self):
+        """ Clears the poem. """
         self.complete_text = []
         self.dirty = False
     
-    def add_lines(self, text, first=False):
+    def add_lines(self, text, ignore_first=False):
+        """
+        Add endline-separated lines in string text to the poem.
+
+        Set ignore_first=True to ignore first line.
+        """
         lines = text.strip().split('\n')
-        start = 0 if first else 1
+        start = 1 if ignore_first else 0
         for line in lines[start:]:
             self.complete_text.append(line + '\n')
         self.dirty = True
+
+    def add_break(self):
+        """ Add a line break to the poem text. """
+        self.complete_text.append('\n')
+
+    def save_to(self, file_path, mode='w'):
+        """
+            Save the poem as a string to a text file at file_path.
+            Returns True on success.
+        """
+        try:
+            with open(file_path, mode) as FILE:
+                FILE.write(self.get_poem())
+        except Exception as exc:
+            print(exc, file=sys.stderr)
+            return False
+        self.dirty = False
+        return True
